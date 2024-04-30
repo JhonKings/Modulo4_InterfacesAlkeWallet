1c36d4d62c201e37aba88369199bce29b313f1c7
Juan Reyes <reyesn.juan@gmail.com>
2024-04-30 01:02:20 -0400

    pantalla 9 con iconos y de todo

---
+++ app/src/main/java/com/example/wallet/fragments/FragmentProfilePage_9.java
@@ -0,0 +1,38 @@
+package com.example.wallet.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wallet.R;
+
+
+public class FragmentProfilePage_9 extends Fragment {
+
+
+
+    public FragmentProfilePage_9() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_page_9, container, false);
+    }
+}
