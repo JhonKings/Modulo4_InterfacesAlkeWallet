5c6c9d4fde0a0e671b81fcd1f1207598be93a27f
Juan Reyes <reyesn.juan@gmail.com>
2024-04-30 12:00:48 -0400

    pantalla fragmento 5 y 6 con todo (svg, png, alineado, etc)

---
+++ app/src/main/java/com/example/wallet/fragments/FragmentHomePageEmptyCase_6.java
@@ -0,0 +1,36 @@
+package com.example.wallet.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wallet.R;
+
+
+public class FragmentHomePageEmptyCase_6 extends Fragment {
+
+
+    public FragmentHomePageEmptyCase_6() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_page_empty_case_6, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/wallet/fragments/FragmentRequestMoney_7.java
@@ -0,0 +1,36 @@
+package com.example.wallet.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wallet.R;
+
+
+public class FragmentRequestMoney_7 extends Fragment {
+
+
+    public FragmentRequestMoney_7() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_request_money_7, container, false);
+    }
+}
